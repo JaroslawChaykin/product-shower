f9eb8504c031474f6aa0457989ed7f3b0070ab0b
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-02-27 14:56:30 +0600

    fix name

--- src/http/http.js
+++ src/http/http.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://api.valantis.store:40000/'
 
 const http = async (body = {}) => {
     const timestamp = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-    const authString = md5(`${import.meta.env.VITE_VLANTIS_API_PASSWORD}_${timestamp}`);
+    const authString = md5(`${import.meta.env.VITE_VALANTIS_API_PASSWORD}_${timestamp}`);
 
     const requestOptions = {
         method: 'POST',
